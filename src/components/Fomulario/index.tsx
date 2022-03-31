8517b7f15b31e9631729a2fb30c2dc36764cfc52
Marcel S Fonseca <marcelsfonseca@gmail.com>
2022-03-31 10:49:18 -0300

    Criando Formulario

--- src/components/Fomulario/index.tsx
+++ src/components/Fomulario/index.tsx
@@ -1,12 +1,22 @@
 import * as React from 'react';
+import Botao from '../Botao';
 
 class Formulario extends React.Component {
     state = {}
     render() {
         return (
             <form action="">
-                <div></div>
-                <div></div>
+                <div>
+                    <label htmlFor='tarefa'>
+                        Adicione um novo estudo
+                    </label>
+                    <input type="text" name='tarefa' id='tarefa' placeholder='O que vc quer esdutar' required /></div>
+                <div>
+                    <label>
+
+                    </label>
+                    <input type="time" /></div>
+                <Botao />
             </form>
         );
     }
