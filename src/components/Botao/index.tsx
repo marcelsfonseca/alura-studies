c4a2294ab7459c5cc797a6f6daa59ba2c244ed60
Marcel S Fonseca <marcelsfonseca@gmail.com>
2022-03-31 10:39:34 -0300

    init

---
+++ src/components/Botao/index.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+class Botao extends React.Component {
+    state = {}
+    render() {
+        return (
+            <button>Criar Pokemon</button>
+        );
+    }
+}
+
+export default Botao;
---
+++ src/components/Fomulario/index.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+
+class Formulario extends React.Component {
+    state = {}
+    render() {
+        return (
+            <form action="">
+                <div></div>
+                <div></div>
+            </form>
+        );
+    }
+}
+
+export default Formulario;
