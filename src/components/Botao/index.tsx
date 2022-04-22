176a94048cb9634475d3b10b4599777a423304ab
Marcel S Fonseca <marcelsfonseca@gmail.com>
2022-04-22 20:59:10 -0300

    Contagem regressiva

--- src/components/Botao/index.tsx
+++ src/components/Botao/index.tsx
@@ -1,12 +1,20 @@
 import * as React from 'react';
 import style from './Botao.module.scss'
 
-class Botao extends React.Component<{type?:"button" | "submit" | "reset" | undefined}> {
+class Botao extends React.Component<{
+    type?: "button" | "submit" | "reset" | undefined,
+    onClick?: () => void
+}> {
     state = {}
     render() {
-        const {type="button"} = this.props;
+        const { type = "button", onClick } = this.props;
         return (
-            <button type={type} className={style.botao}>{this.props.children}</button>
+            <button
+                onClick={onClick}
+                type={type}
+                className={style.botao}
+            >
+                {this.props.children}</button>
         );
     }
 }
